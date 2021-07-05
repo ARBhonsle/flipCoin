f27b4c524d2017974bd8228316e46ea3b10212e6
Aditi Bhonle <aditirbhonsle2824@gmail.com>
2021-07-05 20:11:55 +0530

    extended for multiple coin flips and display number of times heads and tails won

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
-random=$((RANDOM%2))
-if [ $random -eq 1 ]
-then
-	echo "Heads: won"
-else
-	echo "Tails: won"
-fi
+head=0
+tail=0
+for((count=0;count<20;count++))
+do
+	random=$((RANDOM%2))
+	if [ $random -eq 1 ]
+	then
+		echo "Heads: won"
+		((head++))
+	else
+		echo "Tails: won"
+		((tail++))
+	fi
+done
+echo "No of times> Heads won:$head, Tails won: $tail"
