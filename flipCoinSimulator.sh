b14c1f79df8da78244e6414da516f77ae3889345
Aditi Bhonle <aditirbhonsle2824@gmail.com>
2021-07-05 20:27:10 +0530

    modified loop until head or tail won by 21 times, show winner and difference in no of wins

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,16 +1,28 @@
 #!/bin/bash
 head=0
 tail=0
-for((count=0;count<20;count++))
+val=0
+while [ $val -eq 0 ]
 do
 	random=$((RANDOM%2))
 	if [ $random -eq 1 ]
 	then
-		echo "Heads: won"
+		#echo "Heads: won"
 		((head++))
 	else
-		echo "Tails: won"
+		#echo "Tails: won"
 		((tail++))
+	fi
+	if [ $head -eq 21 ]
+	then
+		echo "Heads won over Tails by $(($head-$tail)) times"
+		val=1
+	elif [ $tail -eq 21 ]
+	then
+		echo "Tails won over Heads by $(($tail-$head)) times"
+		val=1
+	else
+		val=0
 	fi
 done
 echo "No of times> Heads won:$head, Tails won: $tail"
